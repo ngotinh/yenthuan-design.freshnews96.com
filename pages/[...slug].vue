fe628574a48462d3422e8ed4d59cb1777e56ef85
ngotinh <kendyngo123@gmail.com>
2023-06-30 15:35:44 +0700

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -17,7 +17,7 @@ if (route.params.slug) {
     id = id[0];
   }
 
-  const apiUrl = `https://military.freshnews95.com/wp-json/wp/v2/posts?slug=${id}`;
+  const apiUrl = `https://design.freshnews96.com/wp-json/wp/v2/posts?slug=${id}`;
   const response = await fetch(apiUrl);
   const json = await response.json();
   //console.log(data);
